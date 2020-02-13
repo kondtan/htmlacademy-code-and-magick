b508a8a1edd1b4cfd408db2a855b1a47491d2379
Tanya <kondrashina00@gmail.com>
2020-02-13 21:43:43 +0300

    Поправлены названия массивов, getRandomInteger возвращён наружу

--- js/setup.js
+++ js/setup.js
@@ -3,8 +3,8 @@
 var WIZARDS_AMOUNT = 4;
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLOR_ARRAY = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLOR_ARRAY = ['black', 'red', 'blue', 'yellow', 'green'];
+var COATS_COLORS_ARRAY = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS_ARRAY = ['black', 'red', 'blue', 'yellow', 'green'];
 var userDialog = document.querySelector('.setup');
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
@@ -14,18 +14,18 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
 document.querySelector('.setup-similar').classList.remove('hidden');
 userDialog.classList.remove('hidden');
 
-// генерируем случайные данные в виде объекта wizard
-var randomWizard = function () {
-  // генерируем случайное число
-  var getRandomInteger = function (array) {
-    return array[Math.floor(Math.random() * array.length)];
-  };
+// генерируем случайное число
+var getRandomInteger = function (array) {
+  return array[Math.floor(Math.random() * array.length)];
+};
 
+// генерируем случайные данные в виде объекта wizard
+var generateRandomWizard = function () {
   var wizard = {};
 
   wizard.name = getRandomInteger(NAMES) + ' ' + getRandomInteger(SURNAMES);
-  wizard.coatColor = getRandomInteger(EYES_COLOR_ARRAY);
-  wizard.eyesColor = getRandomInteger(COAT_COLOR_ARRAY);
+  wizard.coatColor = getRandomInteger(EYES_COLORS_ARRAY);
+  wizard.eyesColor = getRandomInteger(COATS_COLORS_ARRAY);
 
   return wizard;
 };
@@ -46,7 +46,7 @@ var insertWizards = function () {
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < WIZARDS_AMOUNT; i++) {
-    fragment.appendChild(renderWizard(randomWizard()));
+    fragment.appendChild(renderWizard(generateRandomWizard()));
   }
 
   return fragment;
